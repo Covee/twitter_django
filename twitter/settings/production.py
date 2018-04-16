0d1a9f49c6fa0d6438aec18a4b1f07bd960f5fb8
Covee <uniqtop91@gmail.com>
2018-04-17 03:05:56 +0900

    splited the general settings.py and fixed init.py

--- twitter/settings/production.py
+++ twitter/settings/production.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'scv(&wve+x%q$!gp(tuu##i5+qalcn!uylf&@z(%m(h-l++&b6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
