fc36b169f4c58ac7a3bd61a5aece391565ce278f
Covee <uniqtop91@gmail.com>
2018-04-18 02:42:28 +0900

    set environments and staticfiles

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/cobee/Desktop/django_files/twitter_django/v_twitter/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
