7a92396bdc4239564730536e60503255927019d5
Covee <uniqtop91@gmail.com>
2018-04-20 05:58:03 +0900

    form과 admin파일 수정

---
+++ src/tweets/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from .models import Tweet
+
+
+class TweetModelForm(forms.ModelForm):
+	class Meta:
+		model = Tweet
+		fields = [
+			"user",
+			"content",
+		]
+		# exclude = ['user']     # if i dont want this field, use 'exclude'
+
+	def clean_content(self, *args, **kwargs):
+		content = self.cleaned_data.get("content")
+		if content == "abc":
+			raise forms.ValidationError("cannot be ABC")
+		return content
