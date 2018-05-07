9907790b06ca8f925ec296cb67d377a8fc874fdc
Covee <uniqtop91@gmail.com>
2018-05-07 16:01:50 +0900

    follow button toggle 생성

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,8 +4,11 @@ from django.views.generic.base import RedirectView
 
 from .views import (
     UserDetailView,
+    UserFollowView
     )
 
 urlpatterns = [
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'),
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow')
+
 ]
